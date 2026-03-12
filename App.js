e1b184c412bda6d5f4afb9c72407555d707b269c
Enzo Souza <enzomns@hotmail.com>
2026-03-12 10:45:17 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+
+  }
+
+  verificar(){
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <div>
+          <input></input>
+          <input></input>
+          <button onClick={this.verificar}>Acessar</button>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
