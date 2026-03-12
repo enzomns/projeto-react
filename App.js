5aea4f7cea57fb54686d25e121bcd38a61494dd5
Enzo Souza <enzomns@hotmail.com>
2026-03-12 11:45:42 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 class App extends React.Component{
   
   constructor(props){
     super(props);
+  }
 
+  Login(){
+      const [email, setEmail] = useState("");
+      const [senha, setSenha] = useState("");
+      const [mensagem, setMensagem] = useState("");
   }
 
   verificar(){
-
+    if (email === "eduardo.lino@pucpr.br" && senha === "123456"){
+      setMensagem("Acessado com sucesso!");
+    }else{
+      setMensagem("E-mail ou senha invalidos.")
+    }
   }
 
   render() {
@@ -17,11 +26,17 @@ class App extends React.Component{
         <h1>Login</h1>
 
         <div>
+          <input type="email" placeholder="E-mail" value={email}
+          onChange={(e) => setEmail(e.target.value)}></input>
+        </div>
+
+        <div>
           <input></input>
-          <input></input>
+        </div>
+
+        <div>
           <button onClick={this.verificar}>Acessar</button>
         </div>
-        
       </div>
     );
   }
