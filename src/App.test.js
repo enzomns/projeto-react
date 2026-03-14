b17fe1a2a4eec5c5a9f34b6b03300297e9b0d6e0
Enzo Souza <enzomns@hotmail.com>
2026-03-14 13:05:16 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,9 @@ import App from './App';
 
 test('renders app without crashing', () => {
 render(<App />);
+});
 
 test('shows login on the page', () => {
   render(<App />);
   expect(screen.getByText(/login/i)).toBeInTheDocument();
-});
-
 });
